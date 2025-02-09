dde16e14f3176166fbc5d0a88221d70c7226a41f
Creed1k3 <Creed1k3@yandex.ru>
2025-02-09 16:58:18 +0300

    add db mockup

---
+++ app/Models/Specification.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Specification extends Model
+{
+    use HasFactory;
+
+    protected $fillable = ['name', 'category_id'];
+
+    public function category()
+    {
+        return $this->belongsTo(Category::class);
+    }
+
+    public function products()
+    {
+        return $this->belongsToMany(Product::class, 'product_specifications')->withPivot('value');
+    }
+}
